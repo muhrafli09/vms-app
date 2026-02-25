9275a09008fe4c60e04837adb853f6287ad7e613
muhrafli09 <m.rafli.a09@gmail.com>
2026-02-24 21:06:57 -0800

    Fix: Load mail settings from database to config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use BezhanSalleh\PanelSwitch\PanelSwitch;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,5 +32,21 @@ class AppServiceProvider extends ServiceProvider
                 ->canSwitchPanels(fn (): bool => auth()->user()?->admin())
                 ->renderHook('panels::user-menu.before');
         });
+        
+        // Load mail settings from database
+        try {
+            $mailSettings = app(\App\Settings\MailSettings::class);
+            
+            Config::set('mail.default', $mailSettings->mail_driver);
+            Config::set('mail.from.address', $mailSettings->mail_from_address);
+            Config::set('mail.from.name', $mailSettings->mail_from_name);
+            Config::set('mail.mailers.smtp.host', $mailSettings->mail_host);
+            Config::set('mail.mailers.smtp.port', $mailSettings->mail_port);
+            Config::set('mail.mailers.smtp.encryption', $mailSettings->mail_encryption);
+            Config::set('mail.mailers.smtp.username', $mailSettings->mail_username);
+            Config::set('mail.mailers.smtp.password', $mailSettings->mail_password);
+        } catch (\Exception $e) {
+            // Settings not yet configured, use .env defaults
+        }
     }
 }
